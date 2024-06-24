330bf5370a574c9a9fdbdf5649bc393bd8a5abe2
Per Stark <perstark.se@gmail.com>
2024-06-24 21:08:19 +0200

    wip streaming

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use mergil::api;
 use mergil::input::{self, InputResult};
 use mergil::markdown;
+use tokio_stream::StreamExt;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -21,6 +22,10 @@ struct Cli {
     /// Disable Markdown rendering
     #[arg(long, default_value = "false")]
     no_markdown: bool,
+
+    /// Enable streaming output
+    #[arg(long, default_value = "false")]
+    stream: bool,
 }
 
 #[tokio::main]
@@ -56,23 +61,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = api::get_api_key();
     let client = reqwest::Client::new();
     
-    match api::send_api_request(&client, &api_key, &cli.model, &contents, cli.no_markdown).await {
-        Ok(api_response) => {
-            if let Some(choice) = api_response.choices.get(0) {
-                let trimmed_content = choice.message.content.trim_start();
-                if !cli.no_markdown && markdown::is_markdown(trimmed_content) {
-                    markdown::render_markdown(trimmed_content);
-                } else {
-                    println!("{}", trimmed_content);
-                }
-            } else {
-                if cli.debug {
-                    println!("No response choices received from the API");
-                }
+//      match api::send_api_request(&client, &api_key, &cli.model, &contents, cli.no_markdown).await {
+//         Ok(api_response) => {
+//             if let Some(choice) = api_response.choices.get(0) {
+//                 let trimmed_content = choice.message.content.trim_start();
+//                 if !cli.no_markdown && markdown::is_markdown(trimmed_content) {
+//                     markdown::render_markdown(trimmed_content);
+//                 } else {
+//                     println!("{}", trimmed_content);
+//                 }
+//             } else {
+//                 if cli.debug {
+//                     println!("No response choices received from the API");
+//                 }
+//             }
+//         },
+//         Err(e) => {
+//             eprintln!("Error: {}", e);
+//         }
+//     }
+
+//     Ok(())
+// }
+    let mut stream = api::send_api_request(&client, &api_key, &cli.model, &contents, cli.no_markdown, cli.stream).await?;
+
+    while let Some(chunk) = stream.next().await {
+        match chunk {
+            Ok(content) => {
+                markdown::render_markdown(&content);
+                // if !cli.no_markdown && markdown::is_markdown(&content) {
+                //     markdown::render_markdown(&content);
+                // } else {
+                //     println!("NO MARKDOWN");
+                //     print!("{}", content);
+                //     io::stdout().flush().unwrap();
+                // }
             }
-        },
-        Err(e) => {
-            eprintln!("Error: {}", e);
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                break;
+            }
         }
     }
 
